9660b463eaa0cd78d6f17fa7d8dabca0e6104ba9
Moe <musavimoe@gmail.com>
2023-10-01 15:09:52 +0200

    feat: add transition animations

--- src/lib/components/SlidePanel.svelte
+++ src/lib/components/SlidePanel.svelte
@@ -3,6 +3,7 @@
   import Portal from '$lib/components/Portal.svelte';
   import Arrow from './Icon/Arrow.svelte';
   import Overlay from './Overlay.svelte';
+  import { fly } from 'svelte/transition';
 
   const dispatch = createEventDispatcher();
 </script>
@@ -18,7 +19,10 @@
 <Portal>
   <Overlay />
   <div
-    class="fixed right-0 top-0 z-slidePanel h-screen w-full overflow-y-scroll bg-white px-5 pt-16 shadow-slidePanel lg:w-3/4 lg:py-20 lg:px-32"
+    class="fixed right-0 top-0 z-slidePanel h-screen w-full overflow-y-scroll bg-white px-5 pt-16 shadow-slidePanel lg:w-3/4 lg:py-20 lg:px-32
+    transition:fly={{ duration: 500, x: 1000 }}
+    
+    "
   >
     <button
       class="absolute top-5 left-7 text-pastelPurple hover:text-daisyBush"
